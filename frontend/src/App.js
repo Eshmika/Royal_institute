63ea62ac150fa55140cfbe2e4b0ad7de1e8cf078
SamudraW <mattegoda15@gmail.com>
2024-04-22 23:54:19 +0530

    delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,27 +24,7 @@ import AdminForgetPassword from './screen/Admin/Forgetpasswordadmin';
 import AdminProfile from './screen/Admin/AdminProfile';
 import AddTeacher from './screen/Admin/AddTeacher';
 
-import Question from './screen/Question';
-import AddQuestion from './screen/AddQuestion';
-import UpdateQuestion from './screen/UpdateQuestion';
-import MyQuestions from './screen/MyQuestions';
-import FeedbackType from './screen/FeedbackType';
-import TFeedback from './screen/TFeedback';
-import SFeedback from './screen/SFeedback';
-import Feedback from './screen/Feedback';
-import MyFeedbacks from './screen/MyFeedbacks';
-import THQuestion from './screen/THQuestion';
-import TeacherQuestion from './screen/TeacherQuestion';
-import AnswerQ from './screen/AnswerQ';
-import AnswerUpdate from './screen/AnswerUpdate';
-import FAskedQ from './screen/FAskedQ';
-import UpdateTeacherF from './screen/UpdateTeacherF';
-import UpdateSFeedback from './screen/UpdateSFeedback';
-import ViewTeacherFeedback from './screen/ViewTeacherFeedback';
-import ManagerFeedback from './screen/ManagerFeedback';
-import ManagerNFeedback from './screen/ManagerNFeedback';
-import ReplyF from './screen/ReplyF';
-import MFeedbackUpdate from './screen/MFeedbackUpdate';
+
 
 
 import axios from 'axios';
@@ -85,30 +65,7 @@ function App() {
       <Route path="/addteacher" element={<AddTeacher />} />
 
 
-      <Route path='/Question' element={<Question/>}/>
-      <Route path='/AddQuestion' element={<AddQuestion/>}/>
-      <Route path='/MyQuestions' element={<MyQuestions/>}/>
-      <Route path='/UpdateQuestion/:id' element={<UpdateQuestion/>}/>
-
-      <Route path='/THQuestion' element={<THQuestion/>}/>
-      <Route path='/TeacherQuestion' element={<TeacherQuestion/>}/>
-      <Route path='/AnswerQ/:id' element={<AnswerQ/>}/>
-      <Route path='/AnswerUpdate/:id' element={<AnswerUpdate/>}/>
-
-      <Route path='/FAskedQ' element={<FAskedQ/>}/>
-      <Route path='/Feedback' element={<Feedback/>}/>
-      <Route path='/FeedbackType' element={<FeedbackType/>}/>
-      <Route path='/TFeedback' element={<TFeedback/>}/>
-      <Route path='/SFeedback' element={<SFeedback/>}/>
-      <Route path='/MyFeedbacks' element={<MyFeedbacks/>}/>
-      <Route path='/UpdateTeacherF/:id' element={<UpdateTeacherF/>}/>
-      <Route path='/UpdateSFeedback/:id' element={<UpdateSFeedback/>}/>
       
-      <Route path='/ViewTeacherFeedback' element={<ViewTeacherFeedback/>}/>
-      <Route path='/ManagerFeedback' element={<ManagerFeedback/>}/>
-      <Route path='/ManagerNFeedback' element={<ManagerNFeedback/>}/>
-      <Route path='/ReplyF/:id' element={<ReplyF/>}/>
-      <Route path='/MFeedbackUpdate/:id' element={<MFeedbackUpdate/>}/>
             
       
     </Routes>    
